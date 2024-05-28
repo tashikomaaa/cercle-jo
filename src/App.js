9e56cc6f88b114f9d86684346a6b8480637bdef0
aldwin <moutarlier.al@gmail.com>
2024-05-29 00:06:38 +0200

    remove animation for the logo

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vous est offert par <a href="https://lecercle.community/">Le Cercle</a> 
         </a>
       </header>
     </div>
