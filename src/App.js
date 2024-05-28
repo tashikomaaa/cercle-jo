7263895fdf2eb5e2e13aadbacc71da24d3aa307e
aldwin <moutarlier.al@gmail.com>
2024-05-29 00:14:28 +0200

    remove animation for the logo

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import logo from './assets/images/logo/logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://lecercle.community/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vous est offert par Le Cercle
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://lecercle.community/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Vous est offert par Le Cercle
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
