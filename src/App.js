28a51e01bca95df6d392b31f01192a43480b1f8f
DevFlo <34539244+sinckrow@users.noreply.github.com>
2024-05-29 02:22:37 +0200

    add map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './assets/images/logo/logo.png';
 import './App.css';
+import GMapComponent from './components/map';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
+                <GMapComponent />
                 <a
                     className="App-link"
                     href="https://lecercle.community/"
