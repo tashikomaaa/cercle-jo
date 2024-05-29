efbaa30120074141b895c27de888179899486c7a
aldwin <moutarlier.al@gmail.com>
2024-05-29 17:22:51 +0200

    fix

--- src/components/map.js
+++ src/components/map.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import {
     MapContainer, TileLayer, Marker, Popup,
 } from 'react-leaflet';
@@ -9,17 +9,39 @@ import ButtonComponent from "./button";
 
 const MapComponent = () => {
     const mapRef = useRef(null);
-    const position = [48.866667, 2.333333];
 
+    const [userLocation, setUserLocation] = useState([48.866667, 2.333333]);
+    useEffect(() => {
+        // if geolocation is supported by the users browser
+        if (navigator.geolocation) {
+            // get the current users location
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    // save the geolocation coordinates in two variables
+                    const { latitude, longitude } = position.coords;
+                    // update the value of userlocation variable
+                    setUserLocation([latitude, longitude]);
+                },
+                // if there was an error getting the users location
+                (error) => {
+                    console.error('Error getting user location:', error);
+                }
+            );
+        }
+        // if geolocation is not supported by the users browser
+        else {
+            console.error('Geolocation is not supported by this browser.');
+        }
+    }, [])
     return (
         <div>
             <div style={{ position: 'relative', height: '65vh', width: '100vw' }}>
-                <MapContainer center={position} zoom={13} ref={mapRef} style={{ height: "100%", width: "100%" }}>
+                <MapContainer center={userLocation} zoom={13} ref={mapRef} style={{ height: "100%", width: "100%" }}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position}>
+                    <Marker position={userLocation}>
                         <Popup>
                             A pretty CSS3 popup. <br /> Easily customizable.
                         </Popup>
