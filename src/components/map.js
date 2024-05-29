f667d1220c75c2d84ebd97144f67f2524121e285
aldwin <moutarlier.al@gmail.com>
2024-05-29 14:52:32 +0200

    add download

--- src/components/map.js
+++ src/components/map.js
@@ -12,10 +12,7 @@ const MapComponent = () => {
     return (
         // Make sure you set the height and width of the map container otherwise the map won't show
         <MapContainer center={position} zoom={13} ref={mapRef} style={{ height: "50vh", width: "100vw" }}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+            <TileLayer/>
             {/* Icon marker ne s'affiche pas, pourquoi?  */}
             <Marker position={position}>
                 <Popup>
